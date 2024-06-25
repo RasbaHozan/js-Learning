70463b698699abfb4fba72fdd8529302f9fb605d
RasbaHozan <rasbahozan426@gmail.com>
2024-06-25 17:31:48 +0500

    Delete  oddToEven.js

---  oddToEven.js
+++
@@ -1,9 +0,0 @@
-function convertOddToEven(number) {
- if (number % 2 !== 0) {
-      number++;
-    }
-    return number;
-  }
-  let oddNumber = 1;
-  let evenNumber = convertOddToEven(oddNumber);
-  console.log(evenNumber)
