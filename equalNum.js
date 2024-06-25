b0229745a4f01abbb741c35cd8d5ca2236f466a5
RasbaHozan <rasbahozan426@gmail.com>
2024-06-25 17:48:07 +0500

    Update equalNum.js

--- equalNum.js
+++ equalNum.js
@@ -1,12 +1,8 @@
-function checkEqualNumbers(num1,num2) {
-    if (num1 === num2) {
-        return num1 + " is equal to " + num2;
-    } else {
-        return num1 + " is not equal to " + num2;
-    }
+let num1 = 8;
+num2 = 10; 
+if (num1 == num2) {
+    console.log("num1 is equal to num2")
 }
-
-let num1=4;
-let num2=6;
-let result=checkEqualNumbers(num1,num2);
-console.log(result)
+else {
+    console.log("num1 is not equal to num2")
+}
