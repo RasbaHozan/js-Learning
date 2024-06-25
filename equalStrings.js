9cf98cf22087f3b4e05c07c9043b2f7a7c187293
RasbaHozan <rasbahozan426@gmail.com>
2024-06-25 18:47:24 +0500

    Update equalStrings.js

--- equalStrings.js
+++ equalStrings.js
@@ -1,12 +1,7 @@
-function checkEqualStrings(str1,str2) {
-    if (str1 === str2) {
-        return str1 + "is equal to " + str2
-    } else {
-        return str1 + " is not equal to " + str2
-    }
+let str1="english";
+str2 = "maths";
+if (str1 == str2) {
+    console.log("str1 is equal to str2");
+} else {
+    console.log("str1 is not equal to str2");
 }
-
-let str1="English";
-let str2="Maths";
-let result=checkEqualStrings(str1,str2);
-console.log(result)
