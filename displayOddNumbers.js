ccbccd214f490a5798bbcbe005b7296ae25beb23
RasbaHozan <rasbahozan426@gmail.com>
2024-06-26 15:28:48 +0500

    Update displayOddNumbers.js

--- displayOddNumbers.js
+++ displayOddNumbers.js
@@ -1,8 +1,5 @@
-function displayOddNumbers(input) {
-for (let i = 1; i <= 100; i++) {
-    if (i % 2 !== 0) {
+for ( i =0 ; i <=100; i++) {
+    if ( i %2 !==0) {
         console.log(i);
     }
 }
-}
-displayOddNumbers('display the odd numbers')
