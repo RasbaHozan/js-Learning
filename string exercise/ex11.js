73c2e4d2a0c0fac26e1364c7fa5ff4099cfe787d
Rasba Hozan <rasbahozan426@gmail.com>
2024-07-10 12:43:21 +0500

    files.js

--- string exercise/ex11.js
+++ string exercise/ex11.js
@@ -1,27 +1,26 @@
 //write a function to replace the string with substring
-function replaceStr(str1,str2, substring) {
+function replaceStr(str1,str2, substr) {
     let result = "";
     let index = 0;
     for (let i = 0; i < str1.length; i++) {
         let found = true;
-        for (let j = 0; j < substring.length; j++) {
-            if (str1[i + j] !== substring[j]) {
+        for (let j = 0; j < substr.length; j++) {
+            if (str1[i + j] !== substr[j]) {
                 found = false;
                 break;
             }
         }
         if (found==true) {
             result += str2; 
-            i += substring.length - 1;
-        } else {
+            i += substr.length - 1;
+        } 
             result += str1[i];
-        }
     }
     return result;
    }
 let str1 = "hello there";
-let substring = "llo";
+let substr = "llo";
 let str2 = "pop";
 
-let result = replaceStr(str1,str2, substring)
+let result = replaceStr(str1, substr)
 console.log(result);
