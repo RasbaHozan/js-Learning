510e7583104ed635cd88c0080d4094cf94ccbe25
Rasba Hozan <rasbahozan426@gmail.com>
2024-07-11 13:05:23 +0500

    files.js

---
+++ string exercise/sol1.js
@@ -0,0 +1,16 @@
+// write a function to split string into chunks of specific length.
+function solution (str) {
+ let array =[];
+let substring="";
+for ( let i=0; i<str.length;i++) {
+ substring += str[i];
+ if (substring.length == splitLength|| i== str.length-1) {
+  array[array.length] =substring;
+  substring= "";
+ }
+}
+return array ;
+}
+let str = "my name";
+let splitLength=2;
+console.log(solution(str));
