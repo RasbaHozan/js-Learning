4ac017f61cd59c791ded587372da3bd984c02099
unknown <rasbahozan426@gmail.com>
2024-07-22 05:46:14 -0700

    files.json

---
+++ classProblems/1.js
@@ -0,0 +1,58 @@
+class Car {
+    constructor(make, model, year) {
+        this._make = make;
+        this._model = model;
+        this._year = year;
+    }
+
+    get make() {
+        return this._make;
+    }
+
+    set make(make) {
+        this._make = make;
+    }
+
+    get model() {
+        return this._model;
+    }
+
+    set model(model) {
+        this._model = model;
+    }
+
+    get year() {
+        return this._year;
+    }
+
+    set year(year) {
+        this._year = year;
+    }
+}
+
+let myCar = new Car("Honda", "City", 2022);
+
+console.log(myCar.make);  
+console.log(myCar.model); 
+console.log(myCar.year);  
+
+
+
+// now Instantiate multiple Car objects
+let car1 = new Car("Honda", "City", 2022);
+let car2 = new Car("Toyota", "Corolla", 2023);
+let car3 = new Car("Kia", "Sportage", 2024);
+
+// Access properties
+console.log(car1.make); 
+console.log(car2.model); 
+console.log(car3.year);  
+
+// update year property
+car1.year = "2024";
+car2.year = "2023";
+car3.year = 2025;
+
+console.log(car1.year);  
+console.log(car2.year);  
+console.log(car3.year);  
