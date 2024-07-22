1026deb20451e315528208c534301c4c3dbd46fb
unknown <rasbahozan426@gmail.com>
2024-07-21 22:31:53 -0700

    files.json

--- json_data_problems/prob2.js
+++ json_data_problems/prob2.js
@@ -1,36 +1,41 @@
 let jsonData = {
-    "prod": [
-      { "name": "one", "sold": 52 },
-      { "name": "two", "sold": 12 },
-      { "name": "three", "sold": 3 },
-      { "name": "four", "sold": 1 },
-      { "name": "five", "sold": 47 },
-      { "name": "six", "sold": 5 }
-    ]
-  };
-  
-  // Calculate total number of products sold
-  let totalSold = 0;
-  for (let i = 0; i < jsonData.prod.length; i++) {
-    totalSold += jsonData.prod[i].sold;
-  }
-  
+  "prod": [
+    { "name": "one", "sold": 52 },
+    { "name": "two", "sold": 12 },
+    { "name": "three", "sold": 3 },
+    { "name": "four", "sold": 1 },
+    { "name": "five", "sold": 47 },
+    { "name": "six", "sold": 5 }
+  ]
+};
 
-  
-  // Find the product that sold the most
+// Function to calculate total number of products sold
+function totalSold(products) {
+  let totalSold = 0;
+  for (let i = 0; i < products.length; i++) {
+    totalSold += products[i].sold;
+  }
+  return totalSold;
+}
+
+// Function to find the product that sold the most
+function bestSellingProduct(products) {
   let maxSold = 0;
-  let bestSellingProduct = 0;
-  
-  for (let i = 0; i < jsonData.prod.length; i++) {
-    if (jsonData.prod[i].sold > maxSold) {
-      maxSold = jsonData.prod[i].sold;
-      bestSellingProduct = jsonData.prod[i];
+  let bestSellingProd = null;
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].sold > maxSold) {
+      maxSold = products[i].sold;
+      bestSellingProd = products[i];
     }
   }
-  
-  
-  console.log("Total number of products sold");
-  console.log(totalSold);
-  console.log("Product that sold the most");
-  console.log(bestSellingProduct);
-  
+  return bestSellingProd;
+}
+
+let totalProductsSold = totalSold(jsonData.prod);
+console.log("Total number of products sold");
+console.log(totalProductsSold);
+
+let bestProduct = bestSellingProduct(jsonData.prod);
+console.log("Product that sold the most");
+console.log(bestProduct);
