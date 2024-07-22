4c82fa29b26b8361afac7561a15eebbf1d864f6e
unknown <rasbahozan426@gmail.com>
2024-07-21 23:13:55 -0700

    files.json

--- json_data_problems/prob1.js
+++ json_data_problems/prob1.js
@@ -9,7 +9,7 @@ let studentsData = {
             "Address": "NYC"
         },
         {
-            "id": 1,
+            "id": 2,
             "name": "aisha1",
             "age": 19,
             "class": "9",
@@ -17,7 +17,7 @@ let studentsData = {
             "Address": "NYC"
         },
         {
-            "id": 1,
+            "id": 3,
             "name": "aisha2",
             "age": 21,
             "class": "9",
@@ -25,7 +25,7 @@ let studentsData = {
             "Address": "TX"
         },
         {
-            "id": 1,
+            "id": 4,
             "name": "aisha3",
             "age": 21,
             "class": "9",
@@ -35,38 +35,88 @@ let studentsData = {
     ]
 };
 
+function filter1(students) {
+    let filteredStudents = [];
+    for (let i = 0; i < students.length; i++) {
+        let student = students[i];
+        let countGradeF = 0;
+        for (let j = 0; j < student.grades.length; j++) {
+            if (student.grades[j] == "F") {
+                countGradeF++;
+            }
+        }
+        if (countGradeF < 2) {
+            filteredStudents[filteredStudents.length] = student;
+        }
+    }
+    return filteredStudents;
+}
 
-//solution
-//
-
-let filter1 = studentsData.students.filter(student => {
-    let gradeF = student.grades.filter(grade => grade == "F").length;
-    return gradeF < 2;
-});
-console.log("students who didn't get F in any 2 courses");
-console.log(filter1);
-
-
-let filter2= studentsData.students.filter(student => {
-    let gradeA = student.grades.every(grade => grade == "A");
-    return !gradeA;
-});
-console.log("studens who didn't get all A")
-console.log(filter2);
-
-
-let filter3 = studentsData.students.filter(student => student.age > 20);
-
-console.log("students with age > 20")
-console.log(filter3);
+function filter2(students) {
+    let filteredStudents = [];
+    for (let i = 0; i < students.length; i++) {
+        let student = students[i];
+        let foundGradeA = true;
+    
+        for (let j = 0; j < student.grades.length; j++) {
+          
+            if (student.grades[j] == 'A') {
+                foundGradeA = false;
+                break; 
+        }
+    }
+        
+        if (!foundGradeA) {
+            filteredStudents[filteredStudents.length] = student;
+        }
+    }
+    return filteredStudents;
+}
 
 
-let filter4 = studentsData.students.filter(student => student.Address !== "NYC");
 
-console.log("students not living in NYC")
-console.log(filter4);
+function filter3(students) {
+    let filteredStudents = [];
+    for (let i = 0; i < students.length; i++) {
+        let student = students[i];
+        if (student.age > 20) {
+            filteredStudents[filteredStudents.length] = student;
+        }
+    }
+    return filteredStudents;
+}
 
-//now add an array in each student data 
+
+function filter4(students) {
+    let filteredStudents = [];
+    for (let i = 0; i < students.length; i++) {
+        let student = students[i];
+        if (student.Address !== "NYC") {
+            filteredStudents[filteredStudents.length] = student;
+        }
+    }
+    return filteredStudents;
+}
+
+// Applying filters to the studentsJsonData
+let filtered1 = filter1(studentsData.students);
+let filtered2 = filter2(studentsData.students);
+let filtered3 = filter3(studentsData.students);
+let filtered4 = filter4(studentsData.students);
+
+console.log("Students who didn't get F in any two courses");
+console.log(filtered1);
+
+console.log("Students who didn't get all gradeA");
+console.log(filtered2);
+
+console.log("Students age >20");
+console.log(filtered3);
+
+console.log("Students not living in NYC");
+console.log(filtered4);
+
+// Adding an array for courses in each student's data
 let studentsWithCourses = [];
 for (let i = 0; i < studentsData.students.length; i++) {
     let student = studentsData.students[i];
@@ -81,4 +131,6 @@ for (let i = 0; i < studentsData.students.length; i++) {
     };
     studentsWithCourses[studentsWithCourses.length] = studentWithCourses;
 }
+
+console.log("Students with courses for next semester:");
 console.log(studentsWithCourses);
--- json_data_problems/prob2.js
+++ json_data_problems/prob2.js
@@ -1,41 +1,42 @@
 let jsonData = {
-  "prod": [
-    { "name": "one", "sold": 52 },
-    { "name": "two", "sold": 12 },
-    { "name": "three", "sold": 3 },
-    { "name": "four", "sold": 1 },
-    { "name": "five", "sold": 47 },
-    { "name": "six", "sold": 5 }
-  ]
-};
-
-// Function to calculate total number of products sold
-function totalSold(products) {
-  let totalSold = 0;
-  for (let i = 0; i < products.length; i++) {
-    totalSold += products[i].sold;
-  }
-  return totalSold;
-}
-
-// Function to find the product that sold the most
-function bestSellingProduct(products) {
-  let maxSold = 0;
-  let bestSellingProd = null;
-
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].sold > maxSold) {
-      maxSold = products[i].sold;
-      bestSellingProd = products[i];
+    "prod": [
+      { "name": "one", "sold": 52 },
+      { "name": "two", "sold": 12 },
+      { "name": "three", "sold": 3 },
+      { "name": "four", "sold": 1 },
+      { "name": "five", "sold": 47 },
+      { "name": "six", "sold": 5 }
+    ]
+  };
+  
+  // Function to calculate total number of products sold
+  function totalSold(products) {
+    let totalSold = 0;
+    for (let i = 0; i < products.length; i++) {
+      totalSold += products[i].sold;
     }
+    return totalSold;
   }
-  return bestSellingProd;
-}
-
-let totalProductsSold = totalSold(jsonData.prod);
-console.log("Total number of products sold");
-console.log(totalProductsSold);
-
-let bestProduct = bestSellingProduct(jsonData.prod);
-console.log("Product that sold the most");
-console.log(bestProduct);
+  
+  // Function to find the product that sold the most
+  function bestSellingProduct(products) {
+    let maxSold = 0;
+    let bestSellingProd = null;
+  
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].sold > maxSold) {
+        maxSold = products[i].sold;
+        bestSellingProd = products[i];
+      }
+    }
+    return bestSellingProd;
+  }
+  
+  let totalProductsSold = totalSold(jsonData.prod);
+  console.log("Total number of products sold");
+  console.log(totalProductsSold);
+  
+  let bestProduct = bestSellingProduct(jsonData.prod);
+  console.log("Product that sold the most");
+  console.log(bestProduct);
+  
