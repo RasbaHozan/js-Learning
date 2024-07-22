7fc0ad1aeccae57b8b48208931c2bded8cff607b
unknown <rasbahozan426@gmail.com>
2024-07-22 06:01:14 -0700

    files.json

---
+++ classProblems/2.js
@@ -0,0 +1,14 @@
+class MathUtils {
+    static add(a, b) {
+        return a + b;
+    }
+
+    static multiply(a, b) {
+        return a * b;
+    }
+}
+
+let addOutput = MathUtils.add(3, 9);
+console.log(addOutput);
+let multiplicationOutput= MathUtils.multiply(5, 8);
+console.log(multiplicationOutput);
