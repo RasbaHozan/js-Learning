70425ccabab86c9c4693600c1fdc1cd75cafb609
unknown <rasbahozan426@gmail.com>
2024-07-21 23:31:47 -0700

    files.json

--- json_data_problems/prob3.js
+++ json_data_problems/prob3.js
@@ -1,13 +1,19 @@
 let jsonData = {
-    "Book": "The Last Lecture",
-    "Author": "Randy Pauch",
-    "Published": "2007",
-    "Genre": "biography",
-    "Chapters": 15
-  };
-  
-  let String = `"The book "${jsonData.Book.toLowerCase()}"was published in ${jsonData.Published} 
-  by ${jsonData.Author.toLowerCase()}, the book consists of ${jsonData.Chapters} chapters and genre is ${jsonData.Genre}".`;
-  
-  console.log(String);
-  
+  "Book": "The Last Lecture",
+  "Author": "Randy Pausch",
+  "Published": "2007",
+  "Genre": "biography",
+  "Chapters": 15
+};
+
+
+
+function makeStr(data) {
+  //using template literals
+  let bookString = `The book "${data.Book.toLowerCase()}" was published in ${data.Published} by ${data.Author.toLowerCase()}, the book consists of ${data.Chapters} chapters and genre is ${data.Genre}.`;
+
+  return bookString;
+}
+
+let resultString = makeStr(jsonData);
+console.log(resultString);
